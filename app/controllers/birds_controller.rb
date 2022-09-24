856a8eb990b79cafc25085ca51708c8e77f5cd27
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-09-24 16:15:31 +0300

    all

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,26 @@
 class BirdsController < ApplicationController
 
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  # PATCH /birds/:id
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   # GET /birds
   def index
     birds = Bird.all
